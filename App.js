30115dbb858d97164ae6563b37cb3ab3f8fa1038
poorna99122 <46306217+poorna99122@users.noreply.github.com>
2024-07-15 22:42:28 +0530

    Created React App with npx create-react-app and added basic header

---
+++ App.js
@@ -0,0 +1,63 @@
+const Header = ()=>{
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
+const AppLayout = ()=>{
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading = React.createElement(
+//   "h2",
+//   { id: "heading", xyz: "abc" },
+//   "Hii I am Poorna Chandra Rao"
+// );
+
+// console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
---
+++ namasthe-react/src/App.js
@@ -0,0 +1,70 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+const Header = ()=>{
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
+const AppLayout = ()=>{
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+
+export default AppLayout;
+
+
+
+
+
+
+
